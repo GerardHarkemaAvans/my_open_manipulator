07f42a418cb06ecf16a1eab3339af18cc81b9cec
Gerard Harkema <ga.harkema@avans.nl>
2021-12-14 14:41:11 +0100

    20211204

--- install/install.bash
+++ install/install.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cd $HOME/open_manipulator_ws/src
-git  clone https://github.com/GerardHarkemaAvans/open_manipulator.git
-
+#git  clone https://github.com/GerardHarkemaAvans/open_manipulator.git
+sudo apt update
+sudo apt-get install ros-melodic-dynamixel-sdk ros-melodic-dynamixel-workbench*
+sudo apt-get install ros-melodic-robotis-manipulator
 cd $HOME/open_manipulator_ws/
 catkin b
 
