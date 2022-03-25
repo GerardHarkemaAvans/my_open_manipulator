263c5d7d9bddb25d8af5df243e8dc3186793f213
Gerard Harkema <ga.harkema@avans.nl>
2022-03-25 12:31:00 +0100

    modified installation script, made relative

--- install/install.bash
+++ install/install.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-cd $HOME/open_manipulator_ws/src
+#cd $HOME/open_manipulator_ws/src
+cd ../..
 git  clone https://github.com/GerardHarkemaAvans/open_manipulator.git
 
 
-cd $HOME/open_manipulator_ws/src/open_manipulator/install
+#cd $HOME/open_manipulator_ws/src/open_manipulator/install
+cd open_manipulator/install
 ./install.bash
 
 
